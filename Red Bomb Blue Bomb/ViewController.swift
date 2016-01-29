92fe613a13eda5109aa8b50d675e30e350a0f74c
Mike Streff <Mike@mikes-macbook-pro.local>
2016-01-29 13:06:48 -0600

    Initial Commit

---
+++ Red Bomb Blue Bomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red Bomb Blue Bomb
+//
+//  Created by Mike Streff on 1/29/16.
+//  Copyright © 2016 Mike Streff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
