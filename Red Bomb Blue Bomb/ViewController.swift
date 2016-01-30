f3d8bdf565303386934e2398e66db28adbc3d52f
Mike Streff <Mike@Mikes-MacBook-Pro.local>
2016-01-30 15:23:45 -0600

    added files for Bomb

--- Red Bomb Blue Bomb/ViewController.swift
+++ Red Bomb Blue Bomb/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var RedBombPic: UIImageView!
+    @IBOutlet weak var BlueBombPic: UIImageView!
 
+    
+    @IBOutlet weak var HideRedBomb: UIButton!
+    @IBOutlet weak var HideBlueBomb: UIButton!
+    
+    @IBOutlet weak var ExplodeBlue: UIImageView!
+    @IBOutlet weak var ExplodeRed: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideRedBombButton(sender: AnyObject) {
+        RedBombPic.hidden = true
+        }
+    
+    @IBAction func HideBlueBombButton(sender: AnyObject) {
+        BlueBombPic.hidden = true
+        }
 
+    @IBAction func AppearRed(sender: AnyObject) {
+        RedBombPic.hidden = false
+        ExplodeRed.hidden = true
+        
+        }
+    @IBAction func AppearBlue(sender: AnyObject) {
+        BlueBombPic.hidden = false
+        ExplodeBlue.hidden = true
+        }
+    
+    @IBAction func ExplodeBlue(sender: AnyObject) {
+        BlueBombPic.hidden = true
+        ExplodeBlue.hidden = false
+        }
+    
+    @IBAction func ExplodeRed(sender: AnyObject) {
+        RedBombPic.hidden = true
+        ExplodeRed.hidden = false
+    }
 }
+    
+
 
